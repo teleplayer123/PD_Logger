452e38396a9cb24f2d7ba0e789586e24a9c2a1fb
Cole Sashkin <csashceh1024@gmail.com>
2026-01-09 16:15:20 -0800

    update

--- firmware/pd_debug/pd_debug.c
+++ firmware/pd_debug/pd_debug.c
@@ -1438,7 +1438,7 @@ static int handle_command(char *line) {
         // return 1 to tell debug_cli to break loop and return to logging
         return 1;
     } else {
-        usart_printf("Commands:\r\n  Read from register:\t\tr <reg>\r\n  Write to register:\t\tw <reg> <val>\r\n  Read bits in register:\tt <reg> \r\n  Status:\t\t\ts \r\n  Check Rx messages:\t\tc  \r\n  Send SOP message:\t\tx <type> <prole> <drole> <id> <cnt> <rev> <ext>  \r\n  Read FIFO:\t\t\tb  \r\n  Send capabilities:\t\t\tp  \r\n  Quit:\t\t\t\tq  \r\n");
+        usart_printf("Commands:\r\n  Read from register:\t\tr <reg>\r\n  Write to register:\t\tw <reg> <val>\r\n  Read bits in register:\tt <reg> \r\n  Status:\t\t\ts \r\n  Check Rx messages:\t\tc  \r\n  Send SOP message:\t\tx <type> <prole> <drole> <id> <cnt> <rev> <ext>  \r\n  Read FIFO:\t\t\tb  \r\n  Send capabilities:\t\tp  \r\n  Quit:\t\t\t\tq  \r\n");
     }
     return 0;
 }
